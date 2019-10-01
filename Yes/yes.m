de9f9d5a0a5a407a7a8d86b7e60bc7e1a074af7f
Deepti Aggarwal <deeptiagg16@gmail.com>
2019-10-01 21:13:13 +0530

    Added Yes in Objective-C

---
+++ Yes/yes.m
@@ -0,0 +1,12 @@
+/* Yes in Objective-C by Deepti Aggarwal github: [https://github.com/deeptiagg16] */
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{  
+   while(true){
+       
+        NSLog(@"Yes");
+   }
+    return 0;
+}
--- contributors.md
+++ contributors.md
@@ -30,3 +30,4 @@ _(People who have participated in the challenges!)_
 - [rogerthat](https://github.com/rogerthat39)
 - [TheOrangePuff](https:github.com/TheOrangePuff)
 - [Mamat](https://github.com/mamat90)
+- [Deepti Aggarwal](https://github.com/deeptiagg16)
