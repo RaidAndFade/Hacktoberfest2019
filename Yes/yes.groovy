92f19f69f892a29db727cef71c69a59628f0b20f
Rishabh Choudhary <38792773+RisavChd24@users.noreply.github.com>
2019-10-01 13:04:22 +0530

    Create yes.groovy

---
+++ Yes/yes.groovy
@@ -0,0 +1,13 @@
+/*
+  printing yes in groovy language  by 'RisavChd24' https://github.com/RisavChd24 
+*/
+
+
+class rishabh {
+   static void main(String[] args) {
+        while(1) {
+         println('yes');
+         
+      }
+   }
+}
