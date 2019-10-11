b7ef9df46c51d7da6ca884346a602ea48ca4e9cc
LeandraOliveiraS <leandra.silva@ccc.ufcg.edu.br>
2019-10-10 21:25:43 -0300

    yes pull request

---
+++ Yes/Yes.java
@@ -0,0 +1,9 @@
+// By Leandra - https://github.com/LeandraOliveiraS
+
+public class Yes
+{
+    public static void main(String[] args) {
+        while(true)
+            System.out.println("yes");
+    }
+}
